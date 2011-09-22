896e4405e1335a81fcbc28eee26e6bd5eb18157b
Vidmantas <vidmantas@samesystem.dk>
2011-09-22 09:23:37 +0300

    Step for cucumberization

--- lib/cucumberator.rb
+++ lib/cucumberator.rb
@@ -4,3 +4,15 @@ require "cucumberator/cucumberizator"
 After('@cucumberize') do |scenario|
   Cucumberizator.new(self, scenario)
 end
+
+Before do |scenario|
+  @current_cucumberator_scenario = scenario
+end
+
+AfterStep do
+  @current_step = Cucumberizator.extract_current_step(@current_cucumberator_scenario)
+end
+
+Then /^I will write new steps$/ do
+  Cucumberizator.new(self, @current_cucumberator_scenario, @current_step)
+end
--- lib/cucumberator/cucumberizator.rb
+++ lib/cucumberator/cucumberizator.rb
@@ -1,11 +1,25 @@
 class Cucumberizator
-  attr_accessor :world, :scenario, :last_input, :exit_flag
+  attr_accessor :world, :scenario, :step_line, :last_input, :exit_flag
 
   FULL_BACKTRACE = false
 
-  def initialize(world, scenario)
+  # Returns Gherkin::Formatter::Model::Step instance to know where we are exactly (contains line no., keyword, name)
+  # Warning: fragile!
+  def self.extract_current_step(scenario)
+    return nil unless scenario
+
+    scenario.raw_steps.first.feature_element
+      .instance_variable_get("@current_visitor")
+      .instance_variable_get("@listeners").first
+      .instance_variable_get("@current_step")
+      .instance_variable_get("@step")
+      .gherkin_statement
+  end
+
+  def initialize(world, scenario, step = nil)
     @world, @scenario = world, scenario
-    
+    @step_line = step.line if step
+
     check_scenario
     set_autocomplete
     read_input
@@ -16,7 +30,7 @@ class Cucumberizator
   end
 
   def set_autocomplete
-    commands = %w(exit exit-all help last-step save)
+    commands = %w(exit exit-all help last-step save line)
     Readline.completion_proc = proc{|s| commands.grep( /^#{Regexp.escape(s)}/ ) }
   end
 
@@ -44,6 +58,9 @@ class Cucumberizator
     when "save"
       save_last_input
 
+    when "line"
+      save_empty_line
+
     else
       execute_cucumber_step(input)
 
@@ -61,6 +78,7 @@ class Cucumberizator
     puts ":: Available commands:"
     puts "::   save      - saves last step into current feature file"
     puts "::   last-step - display last executed step (to be saved on 'save' command)"
+    puts "::   line      - saves empty line into feature file"
     puts "::   exit      - exits current scenario"
     puts "::   exit-all  - exists whole Cucumber feature"
     puts "::   help      - display this notification"
@@ -70,19 +88,36 @@ class Cucumberizator
     if last_input.blank?
       puts "Hm... nothing to save yet?"
     else
-      File.open(feature_file, 'a') do |f|
-        f.puts((" " * spaces_in_last_line) + last_input) 
-      end
+      string_to_save = (" " * spaces_in_last_input) + last_input
+      save_to_feature_file(string_to_save)
 
       puts "Saved \"#{last_input}\" to #{File.basename(feature_file)}"
       self.last_input = nil
     end
   end
 
+  def save_to_feature_file(string)
+    if step_line
+      feature_file_lines = File.readlines(feature_file)
+      feature_file_lines.insert(step_line, string.to_s+$/) # $/ - default newline separator
+      File.open(feature_file, 'w'){|f| f.puts(feature_file_lines.join) }
+      self.step_line += 1
+    else
+      File.open(feature_file, 'a'){|f| f.puts(string) }
+    end
+  end
+
+  def save_empty_line
+    File.open(feature_file, 'a'){|f| f.puts }
+    puts "New line to #{File.basename(feature_file)}"
+    self.last_input = nil
+  end
+
   def execute_cucumber_step(input)
+    return if input.blank?
+
     self.last_input = input
-    input = "#{input.split.first} '#{input.split[1..-1].join(' ')}'" # making it like >>  When 'I do this and that'
-    world.instance_eval input
+    world.steps(input)
   end
 
 
@@ -92,14 +127,22 @@ class Cucumberizator
     @feature_file ||= File.join(Rails.root, scenario.file_colon_line.split(":").first)
   end
 
-  def spaces_in_last_line
-    for line in File.readlines(feature_file).reverse
-      unless line.blank?
-        spaces = line =~ /\S/
-        return spaces
+  def spaces_in_last_input
+    spaces = 0
+    lines  = File.readlines(feature_file)
+
+    if step_line
+      line = lines[step_line-1]
+    else
+      for l in lines.reverse
+        unless l.blank?
+          line = l
+          break
+        end
       end
     end
 
-    0
+    spaces = line =~ /\S/
+    spaces
   end
 end
